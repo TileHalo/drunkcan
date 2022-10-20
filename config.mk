4f18b300ddcc20d03e91c1279487d2e1f7fcb208
Leo Lahti <leo.m.lahti@aalto.fi>
2022-10-20 17:40:15 +0300

    Changed the style to C99

--- config.mk
+++ config.mk
@@ -8,27 +8,9 @@ PREFIX = /usr/local
 MANPREFIX = $(PREFIX)/share/man
 
 # flags
-_CPPFLAGS = -D_DEFAULT_SOURCE -g
-_CFLAGS = -std=gnu11 -pedantic -Wall -Wextra
-_LDFLAGS =-I/usr/include
-_LDLIBS =
-ifneq "$(and $(TST),$(DBUG))" ""
-CPPFLAGS = $(_CPPFLAGS)
-CFLAGS = $(_CFLAGS) -0s
-LDFLAGS = $(_LDFLAGS) -s
-LDLIBS = $(_LDLIBS) -g
-else ifdef TST
-CPPFLAGS = $(_CPPFLAGS) -g
-LDLIBS = $(_LDLIBS) -lcheck
-LDFLAGS = $(_LDFLAGS)
-LDLIBS = $(_LDLIBS)
-NAME = $(_NAME)-test
-else
-CPPFLAGS = $(_CPPFLAGS) -g
-CFLAGS = $(_CFLAGS)
-LDFLAGS = $(_LDFLAGS)
-LDLIBS = $(_LDLIBS)
-endif
+CFLAGS = -D_DEFAULT_SOURCE -D_POSIX_C_SOURCE=200809L -g -std=c99  -pedantic -Wall -Wextra
+LDFLAGS =-I/usr/include
+LDLIBS =
 
 CC = gcc
 LD = gcc
