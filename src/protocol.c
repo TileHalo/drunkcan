e98d7ebeac19e6744f1349771ad324e85c3984b4
Lahti Leo <leo.m.lahti@aalto.fi>
2022-10-25 15:33:21 +0300

    Added more requirements for protocol

--- src/protocol.c
+++ src/protocol.c
@@ -1,3 +1,6 @@
 /* See LICENSE file for license and copyright details */
 
+#include <stddef.h>
+
+#include <linux/can.h>
 #include "protocol.h"
--- src/protocol.h
+++ src/protocol.h
@@ -1,5 +1,4 @@
 /* See LICENSE file for license and copyright details */
-
 #ifndef DRUNKCAN_PROTOCOL_H
 #define DRUNKCAN_PROTOCOL_H
 enum protocol{
@@ -8,7 +7,11 @@ enum protocol{
 
 struct protocol_conf {
 	enum protocol protocol;
+	void *protocol_state;
 	unsigned int frame_size;
 	int (*id)(int);
+	int (*validate_can)(struct can_frame fr, ...);
+	int (*validate_sock)(void *data, size_t size, ...);
+	int (*process_can)(struct can_frame fr, char *buf, ...);
 };
 #endif
